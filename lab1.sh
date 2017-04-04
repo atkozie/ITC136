db8989e03bd83f114aac868fff8e26e82afcb767
atkozie <atkozie@gmail.com>
2017-04-04 15:09:46 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,7 @@
 # This is a comment 
+
+whoami        # tells you who you are
+pwd           # (print working directory) tells you where you are
+ls            # (list), lists the items in the directory you're in
+cd            # (change directory), changes directories, up or down
+date          # tells you the date and time of the machine
